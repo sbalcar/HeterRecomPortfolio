c058ebd3869af393b3f6c6a3195ee822d6447a3a
sbalcar <mgr.stepan.balcar@gmail.com>
2020-05-21 17:08:40 +0200

    Test of SimulatorOfPortfoliosRecommToUser replaced

--- src/simulation/recommendToUser/simulatorOfPortfoliosRecommToUser.py
+++ src/simulation/recommendToUser/simulatorOfPortfoliosRecommToUser.py
@@ -245,12 +245,8 @@ class SimulationPortfolioToUser:
             userID, portfolioModel, numberOfItems=self._numberOfItems)
 
 
-        #print("nextItemID: " + str(nextItemID))
-        #print("rItemIDs: " + str(rItemIDs))
-
         if not nextItemID in rItemIDs:
             return
-
 
         index:int = rItemIDs.index(currentItemID)
         probOfObserv:float = uProbOfObserv[index]
@@ -344,18 +340,3 @@ class ModelOfIndexes:
 
         return itemNext.getIndexDF()
 
-def test01():
-
-    df:DataFrame = DataFrame({'$a':[2783,2783,2783,3970], '$b':[1909,1396,2901,3408], '$c':[2,4,5,4]})
-    df.columns = [Ratings.COL_USERID, Ratings.COL_MOVIEID, Ratings.COL_RATING]
-    print(df)
-
-    m:ModelOfIndexes = ModelOfIndexes(df)
-    a1:int = m.getNextIndex(2783, 1909)
-    print(a1)
-    a2:int = m.getNextIndex(2783, 1396)
-    print(a2)
-    a3:int = m.getNextIndex(2783, 2901)
-    print(a3)
-
-#test01()
