c2b3033665be1d36ec85ac984d2a535b33f14d8a
skrhakv <vitek.skrhak@seznam.cz>
2020-12-16 20:21:00 +0100

    use lil_matrix for updating and csr_matrix to perform calculations

--- src/recommender/recommenderItemBasedKNN.py
+++ src/recommender/recommenderItemBasedKNN.py
@@ -1,66 +1,66 @@
-
 from recommender.aRecommender import ARecommender  # class
 
-#!/usr/bin/python3
+# !/usr/bin/python3
 
-from pandas.core.frame import DataFrame #class
-from pandas.core.series import Series #class
+from pandas.core.frame import DataFrame  # class
+from pandas.core.series import Series  # class
 
 from sklearn.neighbors import NearestNeighbors
 from sklearn.preprocessing import normalize
 
 from datasets.ml.ratings import Ratings  # class
 
-from history.aHistory import AHistory #class
+from history.aHistory import AHistory  # class
 
-from scipy.sparse import csr_matrix
+from scipy.sparse import csr_matrix, lil_matrix
 import numpy as np
 
 
 class RecommenderItemBasedKNN(ARecommender):
-    def __init__(self, jobID:str, argumentsDict:dict):
+    def __init__(self, jobID: str, argumentsDict: dict):
         if type(argumentsDict) is not dict:
             raise ValueError("Argument argumentsDict is not type dict.")
 
         self._jobID = jobID
         self._argumentsDict: dict = argumentsDict
-        self._KNNs:DataFrame = None
+        self._KNNs: DataFrame = None
         self._distances = None
-        self._sparseRatings:DataFrame = None
+        self._sparseRatings: lil_matrix = None
         self._modelKNN: NearestNeighbors = NearestNeighbors(metric='cosine', algorithm='brute', n_neighbors=20,
                                                             n_jobs=-1)
-        self._lastRatedItemPerUser:DataFrame = None
-        self._itemsDF:DataFrame = None
+        self._lastRatedItemPerUser: DataFrame = None
+        self._itemsDF: DataFrame = None
         self.counter = 0
         self.update_threshold = 100
 
-
-    def train(self, history:AHistory, ratingsTrainDF:DataFrame, usersDF:DataFrame, itemsDF:DataFrame):
+    def train(self, history: AHistory, ratingsTrainDF: DataFrame, usersDF: DataFrame, itemsDF: DataFrame):
         # TODO: Check input/object data integrity!
         self._itemsDF = itemsDF
         cols = ratingsTrainDF['userId']
         rows = ratingsTrainDF['movieId']
         rating = ratingsTrainDF['rating']
-        self._sparseRatings = csr_matrix((rating, (rows, cols)))
-        self._sparseRatings.eliminate_zeros()
+        sparseRatingsCSR: csr_matrix = csr_matrix((rating, (rows, cols)))
+        sparseRatingsCSR.eliminate_zeros()
 
-        self._modelKNN.fit(self._sparseRatings)
+        self._modelKNN.fit(sparseRatingsCSR)
 
-        self._distances ,self.KNNs = self._modelKNN.kneighbors(n_neighbors=100)
+        self._distances, self.KNNs = self._modelKNN.kneighbors(n_neighbors=100)
+
+        self._sparseRatings = sparseRatingsCSR.tolil()
 
         # get last positive feedback from each user
         self._lastRatedItemPerUser = \
-            ratingsTrainDF[ratingsTrainDF['rating'] > 3].sort_values('timestamp')\
+            ratingsTrainDF[ratingsTrainDF['rating'] > 3].sort_values('timestamp') \
                 .groupby('userId').tail(1).set_index('userId').drop(columns=['rating', 'timestamp'])
 
-    def update(self, ratingsUpdateDF:DataFrame):
+    def update(self, ratingsUpdateDF: DataFrame):
         # TODO: Check input/object data integrity!
 
-        row:DataFrame = ratingsUpdateDF.iloc[0]
+        row: DataFrame = ratingsUpdateDF.iloc[0]
 
-        userID:int = row[Ratings.COL_USERID]
-        objectID:int = row[Ratings.COL_MOVIEID]
-        rating:int = row[Ratings.COL_RATING]
+        userID: int = row[Ratings.COL_USERID]
+        objectID: int = row[Ratings.COL_MOVIEID]
+        rating: int = row[Ratings.COL_RATING]
 
         self._sparseRatings[objectID, userID] = rating
 
@@ -69,13 +69,14 @@ class RecommenderItemBasedKNN(ARecommender):
             self._lastRatedItemPerUser['movieId'][userID] = objectID
 
         if self.counter > self.update_threshold:
-            self._modelKNN.fit(self._sparseRatings)
-            self._distances ,self.KNNs = self._modelKNN.kneighbors(n_neighbors=100)
+            sparseRatingsCSR: csr_matrix = self._sparseRatings.tocsr()
+            self._modelKNN.fit(sparseRatingsCSR)
+            self._distances, self.KNNs = self._modelKNN.kneighbors(n_neighbors=100)
             self.counter = 0
         else:
             self.counter += 1
 
-    def recommend(self, userID:int, numberOfItems:int=20, argumentsDict:dict={}):
+    def recommend(self, userID: int, numberOfItems: int = 20, argumentsDict: dict = {}):
         # TODO: Check input/object data integrity!
 
         # Check if user is known
@@ -84,15 +85,16 @@ class RecommenderItemBasedKNN(ARecommender):
             return Series([], index=[])
 
         # Get recommendations for user
-        lastRatedItemFromUser:int = self._lastRatedItemPerUser.loc[userID]['movieId']
-        result:Series = Series(self.KNNs[lastRatedItemFromUser][:numberOfItems])
+        lastRatedItemFromUser: int = self._lastRatedItemPerUser.loc[userID]['movieId']
+        result: Series = Series(self.KNNs[lastRatedItemFromUser][:numberOfItems])
         finalScores = Series(self._distances[lastRatedItemFromUser][:numberOfItems])
         if self._jobID == 'test':
             print("Last visited film:")
             print(self._itemsDF[self._itemsDF['movieId'] == lastRatedItemFromUser])
             print("List of recommendations:")
             for film in result:
-                film_info:DataFrame = self._itemsDF[self._itemsDF['movieId'] == film]
-                print('\t', film_info['movieTitle'].to_string(header=False), film_info['Genres'].to_string(header=False))
+                film_info: DataFrame = self._itemsDF[self._itemsDF['movieId'] == film]
+                print('\t', film_info['movieTitle'].to_string(header=False),
+                      film_info['Genres'].to_string(header=False))
         finalScores = normalize(np.expand_dims(finalScores, axis=0))[0, :]
         return Series(finalScores.tolist(), index=list(result))
