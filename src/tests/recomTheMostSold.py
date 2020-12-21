3ef11ec2c411d79e5e499947bafc0cabf5e047ac
stepan <bc.stepan.balcar@gmail.com>
2020-12-21 10:11:43 +0100

    TheMostSold test update

--- src/tests/recomTheMostSold.py
+++ src/tests/recomTheMostSold.py
@@ -5,7 +5,11 @@ import os
 from recommender.aRecommender import ARecommender #class
 from recommender.recommenderTheMostSold import RecommenderTheMostSold #class
 
+from datasets.aDataset import ADataset #class
+from datasets.datasetRetailrocket import DatasetRetailRocket #class
 from datasets.retailrocket.events import Events #class
+
+from history.historyDF import HistoryDF #class
 
 from pandas.core.frame import DataFrame #class
 
@@ -16,8 +20,10 @@ if __name__ == "__main__":
 
     eventsDF:DataFrame = Events.readFromFile()
 
+    dataset:ADataset = DatasetRetailRocket(eventsDF, DataFrame(), DataFrame())
+
     r:ARecommender = RecommenderTheMostSold("rTheMostSold", {})
-    r.train(None, eventsDF, None, None)
+    r.train(HistoryDF("test"), dataset)
 
     recommendation = r.recommend(1, 20, {})
     print(recommendation)
