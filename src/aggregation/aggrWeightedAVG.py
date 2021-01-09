900181c95e7e8bb92ac207de57d0ab6c93bda897
stepan <bc.stepan.balcar@gmail.com>
2021-01-09 12:19:06 +0100

    AggrWeightedAVG added

---
+++ src/aggregation/aggrWeightedAVG.py
@@ -0,0 +1,120 @@
+#!/usr/bin/python3
+import random
+
+import numpy as np
+import pandas as pd
+
+from numpy.random import beta
+from typing import List
+
+from pandas.core.frame import DataFrame #class
+from pandas.core.series import Series #class
+
+from aggregation.aAggregation import AAgregation #class
+from aggregation.tools.responsibilityDHont import countDHontResponsibility #function
+from aggregation.operators.rouletteWheelSelector import RouletteWheelSelector #class
+
+from history.aHistory import AHistory #class
+from abc import ABC, abstractmethod
+
+from userBehaviourDescription.userBehaviourDescription import UserBehaviourDescription #class
+
+
+class AggrWeightedAVG(AAgregation):
+
+    ARG_SELECTOR:str = "selector"
+
+    def __init__(self, history:AHistory, argumentsDict:dict):
+        if not isinstance(history, AHistory):
+            raise ValueError("Argument history isn't type AHistory.")
+        if type(argumentsDict) is not dict:
+            raise ValueError("Argument argumentsDict isn't type dict.")
+
+        self._history = history
+        self._selector = argumentsDict[self.ARG_SELECTOR]
+
+
+    def update(self, ratingsUpdateDF:DataFrame):
+        pass
+
+
+    # methodsResultDict:{String:pd.Series(rating:float[], itemID:int[])},
+    # modelDF:pd.DataFrame[numberOfVotes:int], numberOfItems:int
+    def run(self, methodsResultDict:dict, modelDF:DataFrame, userID:int, numberOfItems:int = 20):
+
+      # testing types of parameters
+      if type(methodsResultDict) is not dict:
+          raise ValueError("Type of methodsResultDict isn't dict.")
+      if type(modelDF) is not DataFrame:
+          raise ValueError("Type of methodsParamsDF isn't DataFrame.")
+      if list(modelDF.columns) != ['votes']:
+          raise ValueError("Argument methodsParamsDF doen't contain rights columns.")
+      if type(numberOfItems) is not int:
+          raise ValueError("Type of numberOfItems isn't int.")
+
+      if sorted([mI for mI in modelDF.index]) != sorted([mI for mI in methodsResultDict.keys()]):
+        raise ValueError("Arguments methodsResultDict and methodsParamsDF have to define the same methods.")
+      for mI in methodsResultDict.keys():
+          if modelDF.loc[mI] is None:
+              raise ValueError("Argument modelDF contains in ome method an empty list of items.")
+      if numberOfItems < 0:
+        raise ValueError("Argument numberOfItems must be positive value.")
+     
+      # votes number of parties
+      #print("VotesOfPartiesDictI: ", votesOfPartiesDictI)
+      #weighting of individual recommenders
+      resSeries = None
+      for mI in modelDF.index:
+        votes =  modelDF.votes.loc[mI]
+        weightedRes = methodsResultDict[mI] * votes
+        #print(weightedRes)
+        if isinstance(resSeries, pd.Series):
+           resSeries = resSeries.append(weightedRes)
+        else:
+           resSeries = weightedRes 
+
+      #print(resSeries)
+      
+      groupedResults = resSeries.groupby(level=0).sum()    
+      groupedResults.sort_values(ascending = False, inplace = True)
+      #print(groupedResults)
+
+      recommendedItemIDs =  groupedResults.index[:numberOfItems].tolist()
+      return recommendedItemIDs
+
+
+    # methodsResultDict:{String:Series(rating:float[], itemID:int[])},
+    # modelDF:DataFrame<(methodID:str, votes:int)>, numberOfItems:int
+    def runWithResponsibility(self, methodsResultDict:dict, modelDF:DataFrame, userID:int, numberOfItems:int=20):
+
+        # testing types of parameters
+        if type(methodsResultDict) is not dict:
+            raise ValueError("Type of methodsResultDict isn't dict.")
+        for methI in methodsResultDict.values():
+            if type(methI) is not pd.Series:
+                raise ValueError("Type of methodsParamsDF doen't contain Series.")
+        if type(modelDF) is not DataFrame:
+            raise ValueError("Type of methodsParamsDF isn't DataFrame.")
+        if list(modelDF.columns) != ['votes']:
+            raise ValueError("Argument methodsParamsDF doesn't contain rights columns.")
+        if type(numberOfItems) is not int:
+            raise ValueError("Type of numberOfItems isn't int.")
+
+        if sorted([mI for mI in modelDF.index]) != sorted([mI for mI in methodsResultDict.keys()]):
+            raise ValueError("Arguments methodsResultDict and methodsParamsDF have to define the same methods.")
+        for mI in methodsResultDict.keys():
+            if modelDF.loc[mI] is None:
+                raise ValueError("Argument modelDF contains in ome method an empty list of items.")
+        if numberOfItems < 0:
+            raise ValueError("Argument numberOfItems must be positive value.")
+
+
+        aggregatedItemIDs:List[int] = self.run(methodsResultDict, modelDF, numberOfItems)
+        itemsWithResposibilityOfRecommenders = []
+        for item in  aggregatedItemIDs:
+            votesOfItemDictI:dict[str,float] = {mI:methodsResultDict[mI].get(key = item, default = 0) for mI in modelDF.index}
+            itemsWithResposibilityOfRecommenders.append((item, votesOfItemDictI))
+
+        # list<(itemID:int, Series<(rating:int, methodID:str)>)>
+        return itemsWithResposibilityOfRecommenders
+
---
+++ src/tests/testAggrWeightedAVG.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+
+from typing import List
+from pandas.core.frame import DataFrame #class
+
+from aggregation.aggrWeightedAVG import AggrWeightedAVG #class
+
+import pandas as pd
+from history.historyDF import HistoryDF #class
+
+from userBehaviourDescription.userBehaviourDescription import UserBehaviourDescription #class
+from userBehaviourDescription.userBehaviourDescription import observationalStaticProbabilityFnc #function
+from userBehaviourDescription.userBehaviourDescription import observationalLinearProbabilityFnc #function
+
+from aggregation.operators.theMostVotedItemSelector import TheMostVotedItemSelector #class
+
+from aggregation.operators.aDHondtSelector import ADHondtSelector #class
+from aggregation.operators.rouletteWheelSelector import RouletteWheelSelector #class
+from aggregation.operators.theMostVotedItemSelector import TheMostVotedItemSelector #class
+
+from input.inputAggrDefinition import InputAggrDefinition, ModelDefinition  #class
+
+from aggregation.negImplFeedback.aPenalization import APenalization #class
+
+
+def test01():
+    print("Test 01")
+
+    # number of recommended items
+    N = 120
+
+    # method results, items=[1,2,4,5,6,7,8,12,32,64,77]
+    methodsResultDict:dict[str,pd.Series] = {
+          "metoda1":pd.Series([0.2,0.1,0.3,0.3,0.1],[32,2,8,1,4],name="rating"),
+          "metoda2":pd.Series([0.1,0.1,0.2,0.3,0.3],[1,5,32,6,7],name="rating"),
+          "metoda3":pd.Series([0.3,0.1,0.2,0.3,0.1],[7,2,77,64,12],name="rating")
+          }
+    #print(methodsResultDict)
+
+    # methods parametes
+    portfolioModelData:List[tuple] = [['metoda1',100], ['metoda2',80], ['metoda3',60]]
+    portfolioModel:DataFrame = pd.DataFrame(portfolioModelData, columns=["methodID","votes"])
+    portfolioModel.set_index("methodID", inplace=True)
+
+    sumMethodsVotes: float = portfolioModel["votes"].sum()
+    for methodIdI in portfolioModel.index:
+        portfolioModel.loc[methodIdI, "votes"] = portfolioModel.loc[methodIdI, "votes"] / sumMethodsVotes
+
+
+    aggr:AggrWeightedAVG = AggrWeightedAVG(HistoryDF(""), {AggrWeightedAVG.ARG_SELECTOR:TheMostVotedItemSelector({})})
+    #itemIDs:int = aggr.run(methodsResultDict, methodsParamsDF, N)
+    #print(itemIDs)
+    itemIDs:List[tuple] = aggr.runWithResponsibility(methodsResultDict, portfolioModel, N)
+    print(itemIDs)
+
+
+
+if __name__ == "__main__":
+    print("Weighted AVG aggregation")
+
+    test01()
+    # [(1, {'metoda1': 0.0, 'metoda2': 0.0}), (2, {'metoda1': 0, 'metoda2': 0.0}), (3, {'metoda1': 0.0, 'metoda2': 0}), (4, {'metoda1': 0, 'metoda2': 0.0}), (5, {'metoda1': 0.0, 'metoda2': 0}), (6, {'metoda1': 0, 'metoda2': 0.0}), (7, {'metoda1': 0.0, 'metoda2': 0}), (8, {'metoda1': 0, 'metoda2': 0.0}), (9, {'metoda1': 0.0, 'metoda2': 0}), (10, {'metoda1': 0, 'metoda2': 0.0})]
+
+
